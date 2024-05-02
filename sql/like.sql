f6eff17d410eefbcaea13e279a7f4efb616a63e2
qiuhongliang6688 <97734792+nchuhkstu@users.noreply.github.com>
2024-05-02 21:14:20 +0800

    Delete sql directory

--- sql/like.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `like`(
-    likeid int AUTO_INCREMENT PRIMARY KEY ,
-    userid int,
-    likeTime DATETIME
-);
--- sql/photo.sql
+++
@@ -1,7 +0,0 @@
--- 图片表
-CREATE TABLE photo(
-    photoid int AUTO_INCREMENT PRIMARY KEY,
-    url VARCHAR(100),
-    time DATETIME
-);
-INSERT INTO photo (url, time) VALUES ('defaultAvatar.jpg', '2024-04-11 13:06:52');
